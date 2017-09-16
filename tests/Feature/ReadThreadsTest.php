0d7ff668ea28c15a5736e5dec5b39bc83dd2d04c
Dendi Handian <dendihandian@wgs.co.id>
2017-09-17 01:43:19 +0700

    add ReadThreadTest

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -9,6 +9,8 @@ class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $thread;
+
     public function setUp()
     {
         parent::setUp();
@@ -18,19 +20,19 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_view_all_threads()
     {        
-        $response = $this->get('/threads')->assertSee($this->thread->title);
+        $this->get('/threads')->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {        
-        $response = $this->get('/threads/'.$this->thread->id)->assertSee($this->thread->title);
+        $this->get($this->thread->path())->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
-        $response = $this->get('/threads/'. $this->thread->id)->assertSee($reply->body);
+        $this->get($this->thread->path())->assertSee($reply->body);
     }
 }
