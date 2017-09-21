0a8edd41435eec36ec742a5ddff301659f1a559b
Dendi Handian <dendihandian@wgs.co.id>
2017-09-22 00:12:37 +0700

    update ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -23,10 +23,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     ];
 });
 
+$factory->define(App\Channel::class, function($faker){
+	$name = $faker->word;
+	return [
+		'name'	=> $name,
+		'slug' 	=> $name,
+	];
+});
+
 $factory->define(App\Thread::class, function($faker){
 	return [
 		'user_id' => function(){
 			return factory('App\User')->create()->id;
+		},
+		'channel_id' => function(){
+			return factory('App\Channel')->create()->id;
 		},
 		'title'	=> $faker->sentence,
 		'body' 	=> $faker->paragraph,
