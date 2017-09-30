ca1a532eedde86e1f583862b716412fe92b5cd77
Dendi Handian <dendihandian@wgs.co.id>
2017-09-30 19:41:14 +0700

    add channel input, form required validation and  validation errors notification.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,14 +10,34 @@
                     <form method="POST" action="/threads">
                         {{ csrf_field() }}
                         <div class="form-group">
+                            <label for="channel_id">Choose a Channel</label>
+                            <select name="channel_id" id="channel_id" class="form-control" required>
+                                <option value="">Choose One ...</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                        {{ $channel->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" class="form-control" id="title" name="title" placeholder="title">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="title" value="{{ old('title') }}" required>
                         </div>
                         <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea type="text" class="form-control" id="body" name="body" placeholder="body" rows="8"></textarea>
+                            <textarea type="text" class="form-control" id="body" name="body" placeholder="body" rows="8" required></textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary">Publish</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </form>
                 </div>
             </div>
