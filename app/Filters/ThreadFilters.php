83949e08336d8995ce9c17da1cc387987718c5f2
Dendi Handian <dendihandian@wgs.co.id>
2017-10-11 13:10:24 +0700

    add popular filter to ThreadFilters

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -8,12 +8,32 @@ class ThreadFilters extends Filters
 {
 	protected $filters = [
 		'by',
+		'popular',
 	];
 
+	/**
+	 * Filter the query by a given username.
+	 *
+	 * @param string $username
+	 * @return Builder
+	 *
+	*/
 	protected function by($username)
 	{
 		$user = User::where('name', $username)->firstOrFail();
 
 		return $this->builder->where('user_id', $user->id);
+	}
+
+	/**
+	 * Filter the query according to most popular threads.
+	 *
+	 * @return $this
+	 *
+	*/
+	protected function popular()
+	{
+		$this->builder->getQuery()->orders = [];
+		$this->builder->orderBy('replies_count', 'desc');
 	}
 }
