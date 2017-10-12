2126909fcf880ddac775552a932e828a2430d87a
Dendi Handian <dendihandian@wgs.co.id>
2017-10-12 18:21:07 +0700

    add '/replies/{reply}/favorites' route and map to FavoritesController@store

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::post('threads', 'ThreadController@store');
 
 Route::get('threads/{channel}', 'ThreadController@index');
 Route::post('threads/{channel}/{thread}/replies', 'ReplyController@store');
-
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
