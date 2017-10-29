88d89b4aa672da46ad1b647240c7623fd490a44a
Dendi Handian <dendihandian@wgs.co.id>
2017-10-29 21:44:53 +0700

    add feed() static method

--- app/Activity.php
+++ app/Activity.php
@@ -6,10 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    protected $guarded = [];
+	protected $guarded = [];
 
-    public function subject()
-    {
-    	return $this->morphTo();
-    }
+	public function subject()
+	{
+		return $this->morphTo();
+	}
+
+	public static function feed($user, $take = 50)
+	{
+		return static::where('user_id', $user->id)
+			->latest()
+			->with('subject')
+			->take($take)
+			->get()
+			->groupBy(function ($activity) {
+				return $activity->created_at->format('Y-m-d');
+			});
+	}
 }
