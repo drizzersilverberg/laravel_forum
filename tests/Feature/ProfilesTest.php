3b22631263f9c0c6c100b674ea1f3eb0d87669ae
Dendi Handian <dendihandian@wgs.co.id>
2017-10-29 21:49:56 +0700

    modify profiles_display_all_threads_created_by_the_associated_user() test function to use signedIn user

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -21,12 +21,12 @@ class ProfilesTest extends TestCase
     /** @test */
     function profiles_display_all_threads_created_by_the_associated_user()
     {
-    	$user = create('App\User');
+    $this->signIn();
 
-    	$thread = create('App\Thread', ['user_id' => $user->id]);
+    $thread = create('App\Thread', ['user_id' => auth()->id()]);
 
-    	$this->get("/profiles/{$user->name}")
-    		->assertSee($thread->title)
-    		->assertSee($thread->body);
+    $this->get('/profiles/' . auth()->user()->name)
+        ->assertSee($thread->title)
+        ->assertSee($thread->body);
     }
 }
