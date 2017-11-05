c2bfed0a43590d46f0d3ffcef0894b3012ef69e4
Dendi Handian <dendihandian@wgs.co.id>
2017-11-05 23:39:22 +0700

    add reply delete route

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::post('threads', 'ThreadController@store');
 
 Route::get('threads/{channel}', 'ThreadController@index');
 Route::post('threads/{channel}/{thread}/replies', 'ReplyController@store');
+Route::delete('/replies/{reply}', 'ReplyController@destroy');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
