9c2b5189c849123e7199de7577876a26e9903e51
Dendi Handian <dendihandian@wgs.co.id>
2017-11-06 00:54:35 +0700

    add Reply vue component and register it to app.js

---
+++ resources/assets/js/components/Reply.vue
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+	export default {
+		props: ['attributes'],
+
+		data() {
+			return {
+				editing: false,
+				body: this.attributes.body
+			};
+		},
+
+		methods: {
+			update() {
+				axios.patch('/replies/' + this.attributes.id, {
+					body: this.body
+				});
+
+				this.editing = false;
+
+				flash('Updated!');
+			}
+		}
+	}
+</script> 
