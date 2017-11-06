a84d8fd7aad77b99d067003cc0e5e44ff5b12e29
Dendi Handian <dendihandian@wgs.co.id>
2017-11-06 23:51:15 +0700

    appends favoritesCount and isFavorited in Reply

--- app/Reply.php
+++ app/Reply.php
@@ -11,7 +11,14 @@ class Reply extends Model
 	protected $guarded = [];
 
     protected $with = ['owner', 'favorites'];
-	
+
+    /*
+        Once the attribute has been added to the appends list, it will be included in both the model's array and JSON representations.
+
+        Source: https://laravel.com/docs/5.4/eloquent-serialization#appending-values-to-json
+    */
+    protected $appends = ['favoritesCount', 'isFavorited'];
+
     public function owner()
     {
     	return $this->belongsTo(User::class, 'user_id');
