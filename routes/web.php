717b5ecef8c5888e5c7c1e46ce46a4884649621c
Dendi Handian <dendihandian@wgs.co.id>
2017-11-06 23:56:54 +0700

    add delete reply's favorite route

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,6 @@ Route::post('threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::patch('/replies/{reply}', 'ReplyController@update');
 Route::delete('/replies/{reply}', 'ReplyController@destroy');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+Route::delete('/replies/{reply}/favorites', 'FavoritesController@destroy');
 
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
