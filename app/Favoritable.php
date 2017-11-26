7d5ac679ad6f69c8cc60481b172ddba17d1939cb
Dendi Handian <dendihandian@gmail.com>
2017-11-27 00:59:48 +0700

    add bootFavoritable() method and documenting other methods

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -4,11 +4,31 @@ namespace App;
 
 trait Favoritable
 {
+    /**
+     * Boot the trait.
+     */
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
+
+    /**
+     * A reply can be favorited.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
     public function favorites()
     {
         return $this->morphMany(Favorite::class, 'favorited');
     }
 
+    /**
+     * Favorite the current reply.
+     *
+     * @return Model
+     */
     public function favorite()
     {
         $attributes = ['user_id' => auth()->id()];
@@ -18,6 +38,9 @@ trait Favoritable
         }
     }
 
+    /**
+     * Unfavorite the current reply.
+     */
     public function unfavorite()
     {
         $attributes = ['user_id' => auth()->id()];
@@ -25,16 +48,31 @@ trait Favoritable
         $this->favorites()->where($attributes)->delete();
     }
 
+   /**
+     * Determine if the current reply has been favorited.
+     *
+     * @return boolean
+     */
     public function isFavorited()
     {
         return !! $this->favorites->where('user_id', auth()->id())->count();
     }
 
+    /**
+     * Fetch the favorited status as a property.
+     *
+     * @return bool
+     */
     public function getIsFavoritedAttribute()
     {
         return $this->isFavorited();
     }
 
+    /**
+     * Get the number of favorites for the reply.
+     *
+     * @return integer
+     */
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
